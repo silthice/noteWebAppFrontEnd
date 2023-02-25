3ef50f2d60965830fb1d2223060d1d38083ce20d
silthice <ryonlim@hotmail.com>
2023-02-26 02:55:51 +0800

    - simple UI with React, React Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import { Button } from 'react-bootstrap';
-import { Note } from './models/note';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Note as NoteModel } from './models/note';
+import Note from './components/Note';
+import styles from "./styles/NotesPage.module.css"
 
 function App() {
 
-  const [notes, setNotes] = useState<Note[]>([])
+  const [notes, setNotes] = useState<NoteModel[]>([])
 
   useEffect(() => {
 
@@ -24,9 +25,15 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      {JSON.stringify(notes)}
-    </div>
+    <Container>
+      <Row xs={1} md={2} xl={3} className="g-4">
+        {notes.map((note)=> 
+          <Col key={note._id}>
+            <Note note={note} className={styles.note}/>
+          </Col>
+         )}
+      </Row>
+       </Container>
   );
 }
 
---
+++ src/utils/formatDate.ts
@@ -0,0 +1,14 @@
+export function formatDate(dateString: string): string {
+
+    return new Date(dateString)
+    .toLocaleString(
+        "en-US",
+        {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+            hour: "numeric",
+            minute: "numeric"
+        }
+    )
+}
